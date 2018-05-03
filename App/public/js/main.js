ec015de1de93462a0b7c3d0406b93de0d3cc66c3
jmendez <jorge.mendez.ortega@gmail.com>
2018-05-03 02:18:36 -0500

    [UPDATE]: Se actualiza maqueta para pruebas generales

--- App/public/js/main.js
+++ App/public/js/main.js
@@ -83,8 +83,12 @@ document.onreadystatechange = function () {
         _OrcaSlide2.default.config = {
             arrowPrevious: "#arrow_previus",
             arrowNext: "#arrow_next",
+            ctrlStop: "#stop",
+            ctrlPlay: "#play",
             contentItem: "#swipe",
-            time: 1
+            time: 1,
+            timeAutoPlay: 2.5,
+            isInfinite: true
         };
     }
 };
@@ -130,7 +134,7 @@ var OrcaSlide = function (_Utils) {
         /**
          * Genera la transicion de los sliders.
          *
-         * @param  {Boolean} isNext Optional indica el tipo de accion.
+         * @param  {Boolean} isNext (Optional) indica el tipo de accion.
          *
          * @return void.
          */
@@ -141,26 +145,64 @@ var OrcaSlide = function (_Utils) {
             var _configSlide = this.configSlide,
                 active = _configSlide.active,
                 itemWidth = _configSlide.itemWidth,
+                items = _configSlide.items,
                 moveTo = _configSlide.moveTo,
-                time = _configSlide.time;
+                time = _configSlide.time,
+                position = _configSlide.position,
+                isInfinite = _configSlide.isInfinite;
 
 
             var MOVE_TO = isNext ? moveTo : -moveTo;
+            var ACTUAL_POSITION = isNext ? position + 1 : position - 1;
+            var INFINITE = items < ACTUAL_POSITION || ACTUAL_POSITION < 0;
 
             if (active) {
-                this.configSlide.position += isNext ? 1 : -1;
-                this.configSlide.active = false;
-                var counter = 0;
-                var TIMER = setInterval(function () {
-                    _this2.moveToScroll(MOVE_TO);
-                    counter += moveTo;
-                    if (counter >= itemWidth) {
-                        clearInterval(TIMER);
-                        var FULL_MOVE_TO = itemWidth * _this2.configSlide.position;
-                        _this2.moveToScroll(FULL_MOVE_TO, false);
-                        _this2.configSlide.active = true;
-                    }
-                }, time);
+                if (isInfinite && INFINITE) {
+                    this.isInfinite = ACTUAL_POSITION;
+                } else {
+                    this.configSlide.position += isNext ? 1 : -1;
+                    this.configSlide.active = false;
+                    this.isInfinite = ACTUAL_POSITION;
+                    var counter = 0;
+                    var TIMER = setInterval(function () {
+                        _this2.moveToScroll(MOVE_TO);
+                        counter += moveTo;
+                        if (counter >= itemWidth) {
+                            clearInterval(TIMER);
+                            var FULL_MOVE_TO = itemWidth * _this2.configSlide.position;
+                            _this2.moveToScroll(FULL_MOVE_TO, false);
+                            _this2.configSlide.active = true;
+                        }
+                    }, time);
+                }
+            }
+        }
+
+        /**
+         * Permite el manejo de la accion autoPlay.
+         *
+         * @param  {Boolean} (Optional) Indica si el carousel esten autoPlay.
+         * @return {void}.
+         */
+
+    }, {
+        key: "autoPlay",
+        value: function autoPlay() {
+            var _this3 = this;
+
+            var play = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
+
+            this.configSlide.autoPlay = play;
+            var _configSlide2 = this.configSlide,
+                autoPlay = _configSlide2.autoPlay,
+                timeAutoPlay = _configSlide2.timeAutoPlay;
+
+            if (!play && !autoPlay) {
+                clearInterval(this.autoPlayTimer);
+            } else if (play && autoPlay) {
+                this.autoPlayTimer = setInterval(function () {
+                    _this3.animateSlide();
+                }, timeAutoPlay);
             }
         }
 
@@ -175,15 +217,20 @@ var OrcaSlide = function (_Utils) {
     }, {
         key: "displayArrow",
         value: function displayArrow(index) {
-            var _configSlide2 = this.configSlide,
-                arrowNext = _configSlide2.arrowNext,
-                arrowPrevious = _configSlide2.arrowPrevious,
-                items = _configSlide2.items;
+            var _configSlide3 = this.configSlide,
+                autoPlay = _configSlide3.autoPlay,
+                arrowNext = _configSlide3.arrowNext,
+                arrowPrevious = _configSlide3.arrowPrevious,
+                items = _configSlide3.items,
+                isInfinite = _configSlide3.isInfinite;
 
             var DISPLAY_PREVIUS = index > 0 ? "" : "none";
             var DISPLAY_NEXT = items === index ? "none" : "";
             this.displayToggle(arrowNext, DISPLAY_NEXT);
             this.displayToggle(arrowPrevious, DISPLAY_PREVIUS);
+            if (autoPlay && !isInfinite && DISPLAY_NEXT === "none") {
+                this.autoPlay(false);
+            }
         }
 
         // ================================================================= //
@@ -200,42 +247,45 @@ var OrcaSlide = function (_Utils) {
 
     }, {
         key: "startTouch",
+
+
+        /**
+         * Se innicializa el evento touch.
+         *
+         * @return {void} [description]
+         */
         value: function startTouch() {
-            var _this3 = this;
+            var _this4 = this;
 
             var DEVICE = this.isMobile;
-            var _configSlide3 = this.configSlide,
-                contentItem = _configSlide3.contentItem,
-                items = _configSlide3.items,
-                itemWidth = _configSlide3.itemWidth;
-            var position = this.configSlide.position;
+            var _configSlide4 = this.configSlide,
+                contentItem = _configSlide4.contentItem,
+                items = _configSlide4.items;
 
             if (DEVICE !== "desktop") {
-                var clientX = 0;
-                var clientXAuxiliar = 0;
-                var endX = 0;
                 var startX = 0;
-                var X_MAX_AXE = items * itemWidth;
                 contentItem.addEventListener("touchstart", function (action) {
                     var SWIPE = action.changedTouches[0];
-                    if (startX !== 0) {
-                        endX = clientX * -1;
-                    }
                     startX = parseInt(SWIPE.clientX, 10);
                 });
-
                 contentItem.addEventListener("touchmove", function (action) {
                     var SWIPE = action.changedTouches[0];
+                    var direction = "";
                     var swipeX = parseInt(SWIPE.clientX, 10);
-                    clientXAuxiliar = (swipeX - startX + endX) * -1;
-                    if (clientXAuxiliar < 0) {
-                        clientX = 0;
-                    } else if (clientXAuxiliar > X_MAX_AXE) {
-                        clientX = X_MAX_AXE;
+
+                    if (swipeX - startX > 0) {
+                        direction = "right";
                     } else {
-                        clientX = clientXAuxiliar;
+                        direction = "left";
                     }
-                    _this3.moveToScroll(clientX, false);
+
+                    if (direction === "left" && _this4.configSlide.position < items) {
+                        _this4.autoPlay(false);
+                        _this4.animateSlide(true);
+                    } else if (direction === "right" && _this4.configSlide.position > 0) {
+                        _this4.autoPlay(false);
+                        _this4.animateSlide(false);
+                    }
                 });
             }
         }
@@ -254,24 +304,30 @@ var OrcaSlide = function (_Utils) {
             this.configSlide = {
                 arrowNext: "",
                 arrowPrevious: "",
+                autoPlay: false,
                 contentItem: "",
+                ctrlStop: "",
+                ctrlPlay: "",
                 time: 1,
+                timeAutoPlay: 2,
                 isInfinite: false,
                 position: 0,
                 active: false
             };
-
+            this.autoPlayTimer = null;
             Object.assign(this.configSlide, config);
 
             this.validateConfig.setActionButton.resizeSlide.startTouch();
+
+            if (this.configSlide.autoPlay) this.autoPlay();
         }
     }, {
         key: "isInfinite",
         set: function set(index) {
-            var _configSlide4 = this.configSlide,
-                isInfinite = _configSlide4.isInfinite,
-                items = _configSlide4.items,
-                itemWidth = _configSlide4.itemWidth;
+            var _configSlide5 = this.configSlide,
+                isInfinite = _configSlide5.isInfinite,
+                items = _configSlide5.items,
+                itemWidth = _configSlide5.itemWidth;
 
             var RELOAD = (index < 0 || index > items) && index;
             if (isInfinite) {
@@ -296,7 +352,7 @@ var OrcaSlide = function (_Utils) {
     }, {
         key: "resizeSlide",
         get: function get() {
-            var _this4 = this;
+            var _this5 = this;
 
             var CONFIG = this.configSlide;
             var ITEM = this.existFields(CONFIG, "item", null);
@@ -304,11 +360,11 @@ var OrcaSlide = function (_Utils) {
 
             if (ITEM !== null && ELEMENT !== null) {
                 window.addEventListener("resize", function () {
-                    _this4.configSlide.scrollWidth = ELEMENT.scrollWidth;
-                    _this4.configSlide.moveTo = Math.ceil(ITEM.offsetWidth / 256);
-                    _this4.configSlide.itemWidth = ITEM.offsetWidth;
-                    var POST = ITEM.offsetWidth * _this4.configSlide.position;
-                    _this4.moveToScroll(POST, false);
+                    _this5.configSlide.scrollWidth = ELEMENT.scrollWidth;
+                    _this5.configSlide.moveTo = Math.ceil(ITEM.offsetWidth / 256);
+                    _this5.configSlide.itemWidth = ITEM.offsetWidth;
+                    var POST = ITEM.offsetWidth * _this5.configSlide.position;
+                    _this5.moveToScroll(POST, false);
                 });
             }
             return this;
@@ -323,24 +379,27 @@ var OrcaSlide = function (_Utils) {
     }, {
         key: "setActionButton",
         get: function get() {
-            var _this5 = this;
+            var _this6 = this;
 
-            var KEYS = ["arrowNext", "arrowPrevious"];
+            var KEYS = ["arrowNext", "arrowPrevious", "ctrlStop", "ctrlPlay"];
             KEYS.forEach(function (button) {
+                var BUTTON = _this6.configSlide[button];
+                var IS_PLAY = button === "ctrlPlay";
                 var IS_NEXT = button === "arrowNext";
-                var BUTTON = _this5.configSlide[button];
-                BUTTON.addEventListener("click", function () {
-                    var items = _this5.configSlide.items;
-                    var position = _this5.configSlide.position;
+                var callbacks = function callbacks() {};
 
-                    position += IS_NEXT ? 1 : -1;
-                    if (position >= 0 && position <= items) {
-                        _this5.animateSlide(IS_NEXT);
-                        _this5.isInfinite = position;
-                    } else if (items < position || position < 0) {
-                        _this5.isInfinite = position;
-                    }
-                });
+                if (button.includes("ctrl")) {
+                    callbacks = function callbacks() {
+                        _this6.autoPlay(IS_PLAY);
+                    };
+                    _this6.actionButton(BUTTON, callbacks);
+                } else {
+                    callbacks = function callbacks() {
+                        _this6.animateSlide(IS_NEXT);
+                        _this6.autoPlay(false);
+                    };
+                    _this6.actionButton(BUTTON, callbacks);
+                }
             });
             return this;
         }
@@ -356,16 +415,16 @@ var OrcaSlide = function (_Utils) {
     }, {
         key: "validateConfig",
         get: function get() {
-            var _this6 = this;
+            var _this7 = this;
 
             var KEYS = ["arrowNext", "arrowPrevious", "contentItem"];
 
             KEYS.forEach(function (item) {
-                var SELECTOR = _this6.configSlide[item];
-                var ELEMENT = document.querySelector(SELECTOR);
+                var SELECTOR = _this7.configSlide[item];
+                var ELEMENT = _this7.getElementDom(SELECTOR);
 
                 if (ELEMENT) {
-                    _this6.configSlide[item] = ELEMENT;
+                    _this7.configSlide[item] = ELEMENT;
                     if (item === "contentItem") {
                         var ITEM = ELEMENT.children[0] || {};
                         var ITEM_WIDTH = ITEM.offsetWidth || 0;
@@ -374,18 +433,38 @@ var OrcaSlide = function (_Utils) {
                             itemWidth: ITEM_WIDTH,
                             moveTo: Math.ceil(ITEM_WIDTH / 256),
                             scrollWidth: ELEMENT.scrollWidth || 0,
-                            time: _this6.configSlide.time * 1000 / 512,
+                            time: _this7.configSlide.time * 1000 / 512,
                             item: ITEM,
                             content: ELEMENT
                         };
-                        _this6.configSlide.active = NEW_CONFIG.items > 0 && NEW_CONFIG.moveTo > 0;
-                        Object.assign(_this6.configSlide, NEW_CONFIG);
-                        if (!_this6.configSlide.isInfinite) {
-                            _this6.displayToggle(_this6.configSlide.arrowPrevious, "none");
+                        _this7.configSlide.active = NEW_CONFIG.items > 0 && NEW_CONFIG.moveTo > 0;
+                        Object.assign(_this7.configSlide, NEW_CONFIG);
+                        if (!_this7.configSlide.isInfinite) {
+                            _this7.displayToggle(_this7.configSlide.arrowPrevious, "none");
                         }
                     }
                 }
             });
+            return this.validateConfigAutoPlay;
+        }
+    }, {
+        key: "validateConfigAutoPlay",
+        get: function get() {
+            var _configSlide6 = this.configSlide,
+                active = _configSlide6.active,
+                ctrlPlay = _configSlide6.ctrlPlay,
+                ctrlStop = _configSlide6.ctrlStop,
+                timeAutoPlay = _configSlide6.timeAutoPlay;
+
+
+            if (active) {
+                var CONFIG = {
+                    timeAutoPlay: timeAutoPlay * 1000,
+                    ctrlPlay: this.getElementDom(ctrlPlay),
+                    ctrlStop: this.getElementDom(ctrlStop)
+                };
+                Object.assign(this.configSlide, CONFIG);
+            }
             return this;
         }
     }]);
@@ -416,7 +495,29 @@ var Utils = function () {
     }
 
     _createClass(Utils, null, [{
-        key: "displayToggle",
+        key: "actionButton",
+
+        /**
+         * Permite agregar eventos a elementos de la interfaz.
+         *
+         * @param  {object} button Referencia a elemento del dom.
+         * @param  {function} callbacks Funciones a ejecutar.
+         * @param  {String} (Optional) Tipo de accion.
+         *
+         * @return {void}
+         */
+        value: function actionButton(button) {
+            var callbacks = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+            var evento = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "click";
+
+            if (button) {
+                button.addEventListener(evento, function () {
+                    if (typeof callbacks === "function") {
+                        callbacks();
+                    }
+                });
+            }
+        }
 
         /**
          * Permite ocultar y mostar un elemento.
@@ -426,6 +527,9 @@ var Utils = function () {
          *
          * @return {void}
          */
+
+    }, {
+        key: "displayToggle",
         value: function displayToggle(element) {
             var display = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "";
 
@@ -468,6 +572,25 @@ var Utils = function () {
         }
 
         /**
+         * Permite conseguir un elemento del dom identificando si esta vacio o existe.
+         * @param  {String} (Optional) Selector referente al elemento del dom.
+         *
+         * @return {object}.
+         */
+
+    }, {
+        key: "getElementDom",
+        value: function getElementDom() {
+            var selector = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "";
+
+            var domElement = null;
+            if (selector) {
+                domElement = document.querySelector(selector) || domElement;
+            }
+            return domElement;
+        }
+
+        /**
          * Permite identificar el tipo de dispositivo.
          *
          * @type {string}
@@ -481,8 +604,7 @@ var Utils = function () {
          * Permite realizar el movimiento del scroll.
          *
          * @param  {number} pixels Numero de pixeles a desplazar.
-         * @param  {Boolean} isAdd (Optiona) indica si los piexeles se agregan a la
-         *                                   cuenta actual.
+         * @param  {Boolean} isAdd (Optional) indica si los piexeles se agregan a la cuenta actual.
          *
          * @return void.
          */
