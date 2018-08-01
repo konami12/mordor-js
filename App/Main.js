97b80aa51728201dff44f24419e9e5ca315b0c3e
Jmendez <jorge.mendez@televisatim.com>
2018-08-01 18:37:47 -0500

    UPDATE

--- App/Main.js
+++ App/Main.js
@@ -1,9 +1,15 @@
 import OrcaSlide from "./core/orcaslide/source/";
-// import OrcaSlide from "./core/mordor";
+// import OrcaSlide from "orcaslide";
 import "./styles/main.scss";
+
 
 document.onreadystatechange = () => {
     if (document.readyState === "complete") {
+        const demo = (orca, para = {}) => {
+            console.log("----------");
+            console.log(para);
+        };
+
         OrcaSlide.config = {
             arrowPrevious: "#barrow_previus",
             arrowNext: "#barrow_next",
@@ -11,6 +17,38 @@ document.onreadystatechange = () => {
             time: 1,
             isInfinite: true,
             jump: 64,
+            callbacks: [
+                {
+                    callback: (param) => { demo(1, param); },
+                    slide: 1,
+                    next: true,
+                    previus: true,
+                },
+                {
+                    callback: (param) => { demo(2, param); },
+                    slide: 2,
+                    next: true,
+                    previus: true,
+                },
+                {
+                    callback: (param) => { demo(3, param); },
+                    slide: 3,
+                    next: true,
+                    previus: true,
+                },
+                {
+                    callback: (param) => { demo(4, param); },
+                    slide: 4,
+                    next: true,
+                    previus: true,
+                },
+                {
+                    callback: (param) => { demo(5, param); },
+                    slide: 5,
+                    next: true,
+                    previus: true,
+                },
+            ],
         };
         OrcaSlide.config = {
             arrowPrevious: "#carrow_previus",
